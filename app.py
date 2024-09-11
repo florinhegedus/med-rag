52d7eb34d07668d6050a07d3b051242fb8ea51a6
florinhegedus <florinxhgds@gmail.com>
2024-09-11 22:38:18 +0300

    allow to search the db only first time

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main():
     client = OpenAI()
 
     if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-4o-mini"
+        st.session_state["openai_model"] = "gpt-4o"
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -41,19 +41,23 @@ def main():
             st.markdown(message["content"])
 
     if user_prompt := st.chat_input("What is up?"):
-        # Search the DB.
-        results = db.similarity_search_with_score(user_prompt, k=5)
+        if len(st.session_state.messages) == 0:
+            # Search the DB.
+            results = db.similarity_search_with_score(user_prompt, k=5)
 
-        context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+            context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
 
-        # print(context_text)
-        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-        prompt = prompt_template.format(context=context_text, question=user_prompt)
+            # print(context_text)
+            prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+            prompt = prompt_template.format(context=context_text, question=user_prompt)
+        else:
+            prompt = user_prompt
+
         print(prompt)
 
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
-            st.markdown(user_prompt)
+            st.markdown(prompt)
 
         with st.chat_message("assistant"):
             stream = client.chat.completions.create(
